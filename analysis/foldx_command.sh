d842a451c4dd395c5b6946566f130a2a1dd77900
mikkelnrasmussen <s193518@student.dtu.dk>
2023-04-26 16:00:52 +0200

    results: add results from foldx for the AF model with cofactors

--- analysis/foldx_command.sh
+++ analysis/foldx_command.sh
@@ -8,10 +8,10 @@
 # scp high_confidence_AF-p12931.pdb s193518@pupil3.healthtech.dtu.dk:/home/projects/22117_proteins/projects/group14/foldx/
 
 # Running NACCESS on the AF P12931 model
-/home/ctools/naccess-2.1.1/naccess high_confidence_AF-p12931.pdb
+/home/ctools/naccess-2.1.1/naccess AF-P12931-with-cofactors.pdb
 
 # Run FoldX in repair mode:
-/home/ctools/foldx-5.2023/foldx --command=RepairPDB --pdb=high_confidence_AF-p12931.pdb --ionStrength=0.05 --pH=7 --vdwDesign=2 --out-pdb=1 --pdbHydrogens=false  | tee foldx.log
+/home/ctools/foldx-5.2023/foldx --command=RepairPDB --pdb=AF-P12931-with-cofactors.pdb --ionStrength=0.05 --pH=7 --vdwDesign=2 --out-pdb=1 --pdbHydrogens=false  | tee foldx.log
 
 # Create individual list with mutations
 #cat >  individual_list.txt
@@ -22,7 +22,7 @@
 # Ctrl + C
 
 # Run FoldX in build model mode on the repaired model and introduce mutations:
-/home/ctools/foldx-5.2023/foldx --command=BuildModel --pdb=high_confidence_AF-p12931_Repair.pdb --mutant-file=individual_list.txt --ionStrength=0.05 --pH=7 --vdwDesign=2 --out-pdb=1 --pdbHydrogens=false --numberOfRuns=5
+/home/ctools/foldx-5.2023/foldx --command=BuildModel --pdb=AF-P12931-with-cofactors_Repair.pdb --mutant-file=individual_list.txt --ionStrength=0.05 --pH=7 --vdwDesign=2 --out-pdb=1 --pdbHydrogens=false --numberOfRuns=5
 
 # Copy the repair runfile template and mutation list to the current folder on the server:
 cp /home/projects/22117_proteins/lecture4_exercise/group0/mutatex_templates/mutatex/templates/foldxsuite5/repair_runfile_template.txt .
@@ -30,7 +30,14 @@ cp /home/projects/22117_proteins/lecture4_exercise/group0/mutatex_templates/muta
 cp /home/projects/22117_proteins/lecture4_exercise/group0/mutatex_templates/mutatex/templates/mutation_list.txt .
 
 # Run mutateX using the list of mutations:
-nohup mutatex high_confidence_AF-p12931.pdb -p 3 -m mutation_list.txt -x /home/ctools/foldx-5.2023/foldx -f suite5 -R repair_runfile_template.txt -M mutate_runfile_template.txt -q poslist.txt -c -L -l -v & 
+nohup mutatex AF-P12931-with-cofactors.pdb -p 3 -m mutation_list.txt -x /home/ctools/foldx-5.2023/foldx -f suite5 -R repair_runfile_template.txt -M mutate_runfile_template.txt -q poslist.txt -c -L -l -v & 
+
+# Create poslist.txt file:
+#cat >  poslist.txt
+#RA208
+#RA391
+#AA421
+#EA527
 
 # Extract results from the mutateX output:
-/home/ctools/anaconda3_2021.11/bin/ddg2excel -p high_confidence_AF-p12931.pdb -l mutation_list.txt -q poslist.txt -d results/mutation_ddgs/final_averages/ -F csv
+/home/ctools/anaconda3_2021.11/bin/ddg2excel -p AF-P12931-with-cofactors.pdb -l mutation_list.txt -q c.txt -d results/mutation_ddgs/final_averages/ -F csv
