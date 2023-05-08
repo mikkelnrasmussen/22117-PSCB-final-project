18e8a847aaf8f0ce6352cdfa1caae55c09aa0755
jackfriskDTU <s194527@student.dtu.dk>
2023-05-08 11:53:16 +0200

    update: run_foldx.sh for cabsflexmodels

--- analysis/Cabsflex/AF/foldx/run_foldx.sh
+++ analysis/Cabsflex/AF/foldx/run_foldx.sh
@@ -4,34 +4,31 @@
 FOLDX_BIN=/home/ctools/foldx-5.2023/foldx
 
 # Set the working directory to the current directory
-WORK_DIR=$(pwd)
+WORK_DIR=/net/pupil1/home/projects/22117_proteins/projects/group14/CABSflex/AF/foldx
 
 # Loop through each subdirectory
 for dir in mut1 mut2 mut3 mut4 Shrek; do
   echo "Processing directory: ${dir}"
   
-  #mkdir "${dir}_outs"
   # Move to the subdirectory
-  cd "../${dir}"
+  cd "../${dir}/output_pdbs"
   
   # Initialize an empty list of input PDB files
-  pdb_files=""
+  touch pdb_files.tmp
 
-  # Loop through model0.pdb to model10.pdb and add them to the pdb_files list
-  for i in {0..10}; do
-    pdb_files+="output_pdbs/model${i}.pdb"
-    
-    # Add a comma separator, except for the last file in the list
-    if [ $i -ne 10 ]; then
-      pdb_files+=","
-    fi
+  # Loop through model0.pdb to model10.pdb and add them to the pdb_files.tmp file
+  for i in {0..9}; do
+    echo "model_${i}.pdb" >> pdb_files.tmp
   done
   
   # Run FoldX Stability command
-  $FOLDX_BIN --command=Stability --ionStrength=0.05 --pH=7 --vdwDesign=2 --out-pdb=1 --pdbHydrogens=false --pdb-list=${pdb_files} --output-dir="${WORK_DIR}/${dir}_outs"
+  $FOLDX_BIN --command=Stability --ionStrength=0.05 --pH=7 --vdwDesign=2 --pdbHydrogens=false --pdb-list=pdb_files.tmp --output-dir="${WORK_DIR}/${dir}_outs"
   
+  #rm pdb_files.tmp
+
   echo "Finished processing directory: ${dir}"
+
+  # Move back to the initial working directory
+  cd "${WORK_DIR}"
 done
 
-# Move back to the initial working directory
-cd "${WORK_DIR}"
